10b095d551480c7960183b3f19d7e3d73ff27076
Vishal Gupta <ims.vishal@gmail.com>
2026-08-13 20:07:23 +0530

    upgrades

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBGducZcsg7raSbBj3Y4Gud3hHuYnT_abY",
+  authDomain: "aaj-kya-khayenge.firebaseapp.com",
+  projectId: "aaj-kya-khayenge",
+  storageBucket: "aaj-kya-khayenge.firebasestorage.app",
+  messagingSenderId: "706369187613",
+  appId: "1:706369187613:web:f6c163d3a90ea8d8ecfbf4",
+  measurementId: "G-Z3SE7EJC1D"
 };
 
 // Initialize Firebase
